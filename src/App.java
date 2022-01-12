b70756bdf9f25c05bc8f5ceb5a161ab4a0d1100b
EmilioKenjiHK <emilio.her.kur@gmail.com>
2022-01-12 18:19:10 +0000

    YAYA

--- src/App.java
+++ src/App.java
@@ -3,5 +3,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        //PARA ACTUALIZAR EN GITHUB
+        // git add . 
+        // git commit -m "MENSAJE"
+        // git push
     }
 }
